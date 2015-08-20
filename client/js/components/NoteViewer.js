4da703b64b2ece0d0ca9cc6922902558f965f554
Harald Held <harald.held@gmail.com>
2015-08-20 09:30:57 +0200

    Nicely displaying the creation and modification dates.

--- client/js/components/NoteViewer.js
+++ client/js/components/NoteViewer.js
@@ -16,9 +16,25 @@ class NoteViewer extends React.Component {
             return (<span key={i} className="label label-default" style={{marginLeft: 10}}>{tag}</span>);
         });
 
+        const creationDateBadge = (
+            <span className="badge" style={{marginLeft: 5}}>{new Date(this.props.note.creationDate).toLocaleString()}</span>
+        );
+
+        const modDateBadge = (
+            <span className="badge" style={{marginLeft: 5}}>{new Date(this.props.note.lastModDate).toLocaleString()}</span>
+        );
+
         return (
             <div className="panel panel-default">
-                <div className="panel-heading">{this.props.note.title}</div>
+                <div className="panel-heading">
+                    {this.props.note.title}
+                </div>
+                <div className="pull-right" style={{marginLeft: 10}}>
+                    <ul className="list-group">
+                        <li className="list-group-item list-group-item-info">Created {creationDateBadge}</li>
+                        <li className="list-group-item list-group-item-info">Last modified {modDateBadge}</li>
+                    </ul>
+                </div>
                 <div className="panel-body" dangerouslySetInnerHTML={{__html: noteHtml}} />
                 <div className="panel-footer">{tags}</div>
             </div>
